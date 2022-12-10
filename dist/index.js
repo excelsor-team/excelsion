7c46d2c428a3ddb285b45196812036d59479b1c9
DidhacK <didhack@didhack.io>
2022-12-10 19:19:56 +0100

    Remove dist folder

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Excelsion = void 0;
-var excelsion_1 = require("./excelsion");
-Object.defineProperty(exports, "Excelsion", { enumerable: true, get: function () { return excelsion_1.Excelsion; } });
